4ecb145161fefa3e5268c436fc91a106023e90a2
eduardo <eduardinhop@gmail.com>
2026-09-12 12:45:20 -0400

    Requerimientos: Ajustes en algoritmo asignación de carga.

--- src/services/shipments.service.ts
+++ src/services/shipments.service.ts
@@ -37,59 +37,66 @@ export class ShipmentsService {
 
       let vehicle_number: number = 0;
       let total_weight_kg: number = 0;
+      const array_messages: any[] = [];
       const array_vehicles: any[] = [];
       let array_shipments: any[] = [];
       for (const shipment of json_shipments) {
-        array_shipments.push({
-          shipment_id: shipment.id,
-          tracking_code: shipment.guide_code,
-          weight_kg: Number(shipment.weight_kg),
-        });
-
-        total_weight_kg = total_weight_kg + Number(shipment.weight_kg);
-
-        if (total_weight_kg >= parameters.vehicle_capacity) {
-          vehicle_number = vehicle_number + 1;
-
-          let shipments = array_shipments;
-          let total_weight = total_weight_kg;
-          let remaining_capacity =
-            parameters.vehicle_capacity - total_weight_kg;
-          if (total_weight_kg > parameters.vehicle_capacity) {
-            shipments = array_shipments.slice(0, -1);
-
-            total_weight = total_weight_kg - Number(shipment.weight_kg);
-
-            remaining_capacity =
-              parameters.vehicle_capacity -
-              (total_weight_kg - Number(shipment.weight_kg));
-          }
-
-          array_vehicles.push({
-            vehicle_number: vehicle_number,
-            shipments: shipments,
-            total_weight_kg: total_weight,
-            remaining_capacity: remaining_capacity,
-          });
-
-          total_weight_kg = Number(shipment.weight_kg);
-          array_shipments = [];
+        if (Number(shipment.weight_kg) > parameters.vehicle_capacity) {
+          array_messages.push(
+            'El envío ' +
+              shipment.guide_code +
+              ' supera la capacidad del vehículo',
+          );
+        } else {
           array_shipments.push({
             shipment_id: shipment.id,
             tracking_code: shipment.guide_code,
             weight_kg: Number(shipment.weight_kg),
           });
-        }
-      }
 
-      let total_weight_kg_used = 0;
-      for (const vehicle of array_vehicles) {
-        total_weight_kg_used = total_weight_kg_used + vehicle.total_weight_kg;
+          total_weight_kg = total_weight_kg + Number(shipment.weight_kg);
+
+          if (total_weight_kg >= parameters.vehicle_capacity) {
+            vehicle_number = vehicle_number + 1;
+
+            let shipments = array_shipments;
+            let total_weight = total_weight_kg;
+            let remaining_capacity =
+              parameters.vehicle_capacity - total_weight_kg;
+            if (total_weight_kg > parameters.vehicle_capacity) {
+              shipments = array_shipments.slice(0, -1);
+
+              total_weight = total_weight_kg - Number(shipment.weight_kg);
+
+              remaining_capacity =
+                parameters.vehicle_capacity -
+                (total_weight_kg - Number(shipment.weight_kg));
+
+              total_weight_kg = Number(shipment.weight_kg);
+              array_shipments = [];
+              array_shipments.push({
+                shipment_id: shipment.id,
+                tracking_code: shipment.guide_code,
+                weight_kg: Number(shipment.weight_kg),
+              });
+            } else {
+              total_weight_kg = 0;
+              array_shipments = [];
+            }
+
+            array_vehicles.push({
+              vehicle_number: vehicle_number,
+              shipments: shipments,
+              total_weight_kg: total_weight,
+              remaining_capacity: remaining_capacity,
+            });
+          }
+        }
       }
 
       if (
         total_weight_kg <= parameters.vehicle_capacity &&
-        json_shipments.length > 0
+        array_shipments.length > 0
       ) {
         array_vehicles.push({
           vehicle_number: vehicle_number + 1,
@@ -97,8 +104,11 @@ export class ShipmentsService {
           total_weight_kg: total_weight_kg,
           remaining_capacity: parameters.vehicle_capacity - total_weight_kg,
         });
+      }
 
-        total_weight_kg_used = total_weight_kg;
+      let total_weight_kg_used = 0;
+      for (const vehicle of array_vehicles) {
+        total_weight_kg_used = total_weight_kg_used + vehicle.total_weight_kg;
       }
 
       response.status(HttpStatus.OK).json({
@@ -109,6 +119,7 @@ export class ShipmentsService {
             vehicles: array_vehicles,
             total_vehicles_used: array_vehicles.length,
             total_weight_kg: total_weight_kg_used,
+            messages: array_messages,
           },
         ],
       });
